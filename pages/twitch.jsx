aab5ed6b1edc33aee8a622e13bb97ea1f85fb773
gonzaloratti1 <gonzaloratti1@gmail.com>
2023-01-22 22:33:05 -0300

    projects

--- pages/twitch.jsx
+++ pages/twitch.jsx
@@ -17,7 +17,7 @@ const twitch = () => {
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Twitch UI</h2>
+          <h2 className='py-2'>Bartender App</h2>
           <h3>Next JS / Tailwind / Next Auth</h3>
         </div>
       </div>
